ad63290ee25cd5f5908a5847507055d745ca80f2
Jed Rojas <jedrojas@Jeds-MacBook-Pro.local>
2021-01-24 12:21:47 -0500

    initial commit

--- src/components/hooks/user-metadata-hooks.ts
+++ src/components/hooks/user-metadata-hooks.ts
@@ -2,16 +2,17 @@ import { useAuth0 } from '@auth0/auth0-react';
 import fetch from 'node-fetch';
 import { useEffect, useState } from 'react';
 
-export const useUserMetaData = () => {
+export const useGetUserMetaData = () => {
   const { user, getAccessTokenSilently } = useAuth0();
   //   TODO: type this
   const [userInfo, setUserInfo] = useState<any>();
-  const [isFirstLogin, setIsFirstLogin] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   //   TODO: store this in auth provider or something
   const domainName = "https://dev-k8hhju21.us.auth0.com";
 
   useEffect(() => {
+    setLoading(true);
     const fetchData = async () => {
       try {
         const accessToken = await getAccessTokenSilently({
@@ -26,19 +27,64 @@ export const useUserMetaData = () => {
               "content-type": "application/json",
             },
           }
-        );
-        setUserInfo(await getMetadata.json());
+        )
+          .then((res) => {
+            setLoading(false);
+            return res.json();
+          })
+          .catch((e) => {
+            setLoading(false);
+            console.log("-error getting user info-");
+            return e;
+          });
+        setUserInfo(getMetadata);
       } catch (e) {
         throw e;
       }
     };
     if (user) {
       fetchData();
-    } else {
-      setIsFirstLogin(true);
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [getAccessTokenSilently, user]);
 
-  return { userInfo, isFirstLogin };
+  return { userInfo, loading };
+};
+
+export const useUpdateUserMetadata = () => {
+  const { user, getAccessTokenSilently } = useAuth0();
+
+  const updateUser = async (firstName: string, lastName: string) => {
+    let accessToken;
+
+    const payload = {
+      user_metadata: {
+        first_name: firstName,
+        last_name: lastName,
+      },
+    };
+
+    try {
+      accessToken = await getAccessTokenSilently({
+        // TODO: pull this out to config file or something
+        audience: "https://dev-k8hhju21.us.auth0.com/api/v2/",
+        scope: "read:current_user update:current_user_metadata",
+      });
+      console.log("--got access token token--", accessToken);
+    } catch (e) {
+      throw e;
+    }
+
+    await fetch(`https://dev-k8hhju21.us.auth0.com/api/v2/users/${user.sub}`, {
+      method: "PATCH",
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    })
+      .then((res) => console.log("--successfully updated user--", user))
+      .catch((e) => console.log("--error--", e));
+  };
+
+  return { updateUser };
 };
